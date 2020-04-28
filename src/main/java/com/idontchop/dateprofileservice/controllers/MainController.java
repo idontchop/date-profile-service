d18e26f7a26ecb9f7806023749e684bc223b5cea
Nate <nate@idontchop.com>
2020-04-27 20:26:41 -0700

    reduce endpoint

--- src/main/java/com/idontchop/dateprofileservice/controllers/MainController.java
+++ src/main/java/com/idontchop/dateprofileservice/controllers/MainController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,7 +49,7 @@ public class MainController {
 	 * @param reduceRequest
 	 * @return
 	 */
-	@GetMapping(value = "/reduce")
+	@PostMapping(value = "/api/reduce")
 	public List<String> reduce(@RequestBody @Valid ReduceRequest reduceRequest) {
 		return reduceService.reduceByTraitSelections(reduceRequest.getPotentials(),
 				reduceRequest.getSelections() );
